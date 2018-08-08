c47b9aa8dc1565024c053740d33b5d5406547065
Kevin Albertson <kevin.albertson@10gen.com>
2018-08-08 17:21:13 -0400

    apm applied

--- src/mongocxx/client.cpp
+++ src/mongocxx/client.cpp
@@ -21,6 +21,7 @@
 #include <mongocxx/exception/operation_exception.hpp>
 #include <mongocxx/exception/private/error_category.hh>
 #include <mongocxx/exception/private/mongoc_error.hh>
+#include <mongocxx/options/private/apm.hh>
 #include <mongocxx/options/private/ssl.hh>
 #include <mongocxx/private/client.hh>
 #include <mongocxx/private/client_session.hh>
@@ -57,8 +58,17 @@ client::client(const class uri& uri, const options::client& options) {
         // Shouldn't happen after checks above, but future libmongoc's may change behavior.
         throw exception{error_code::k_invalid_parameter, "could not construct client from URI"};
     }
-
+    // Temporarily have to move this up here for the CR. This is fixed in CXX-1614
     _impl = stdx::make_unique<impl>(std::move(new_client));
+
+    if (options.apm_opts()) {
+        _impl->listeners = *options.apm_opts();
+        auto callbacks = options::make_apm_callbacks(_impl->listeners);
+        // We cast the APM class to a void* so we can pass it into libmongoc's context.
+        // It will be cast back to an APM class in the event handlers.
+        auto context = static_cast<void*>(&(_impl->listeners));
+        libmongoc::client_set_apm_callbacks(new_client, callbacks.get(), context);
+    }
 
 #if defined(MONGOCXX_ENABLE_SSL) && defined(MONGOC_ENABLE_SSL)
     if (options.ssl_opts()) {
