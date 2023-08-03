23c8bd33014acc9a1107b0d05e351cbf819b95a2
Kevin Albertson <kevin.albertson@mongodb.com>
2023-08-03 15:38:51 -0400

    ignore NamespaceNotFound

--- src/mongocxx/test/search_index_view.cpp
+++ src/mongocxx/test/search_index_view.cpp
@@ -216,10 +216,11 @@ TEST_CASE("atlas search indexes prose tests", "") {
         std::cout << "update one SUCCESS" << std::endl;
     }
 
-    // SECTION("drop one suppress namespace not found") {
-    //     bsoncxx::oid id;
-    //     auto coll = db[id.to_string()];
-    //     coll.search_indexes().drop_one("apples");
-    // }
+    SECTION("drop one suppress namespace not found") {
+        bsoncxx::oid id;
+        auto coll = db[id.to_string()];
+        auto siv = coll.search_indexes();
+        siv.drop_one("apples");
+    }
 }
 }  // namespace
