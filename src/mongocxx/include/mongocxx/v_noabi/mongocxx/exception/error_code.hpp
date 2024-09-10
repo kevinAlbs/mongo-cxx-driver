63596e4b536eb2060f6774a36b1af594b8d45638
Kevin Albertson <kevin.albertson@mongodb.com>
2024-09-10 13:26:20 -0400

    format

--- src/mongocxx/include/mongocxx/v_noabi/mongocxx/exception/error_code.hpp
+++ src/mongocxx/include/mongocxx/v_noabi/mongocxx/exception/error_code.hpp
@@ -107,7 +107,7 @@ enum class error_code : std::int32_t {
     // A default-constructed or moved-from mongocxx::v_noabi::search_index_view object has been
     // used.
     k_invalid_search_index_view,
-    
+
     /// Acquired a NULL mongocxx::v_noabi::client object.
     k_invalid_client_acquired,
 
