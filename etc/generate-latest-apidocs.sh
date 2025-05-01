7593f22cfb1639fdcc3400a65080ec5d6c8b4d0d
Kevin Albertson <kevin.albertson@mongodb.com>
2025-05-01 17:58:55 -0400

    fix sed command for macOS: part 2

--- etc/generate-latest-apidocs.sh
+++ etc/generate-latest-apidocs.sh
@@ -61,8 +61,8 @@ else
   #  - set OUTPUT_DIRECTORY to `build/docs/api/mongocxx-<version>`.
   #  - set PROJECT_NUMBER to `<version>`.
   sed -i '' \
-    -e "s|^OUTPUT_DIRECTORY\s*=\s*.*$|OUTPUT_DIRECTORY = ${output_directory:?}|g" \
-    -e "s|^PROJECT_NUMBER\s*=\s*.*$|PROJECT_NUMBER = ${LATEST_VERSION:?}|g" \
+    -e "s|^OUTPUT_DIRECTORY[ \t]*=[ \t]*.*$|OUTPUT_DIRECTORY = ${output_directory:?}|g" \
+    -e "s|^PROJECT_NUMBER[ \t]*=[ \t]*.*$|PROJECT_NUMBER = ${LATEST_VERSION:?}|g" \
     "${scratch_dir:?}/Doxyfile"
 fi
 
