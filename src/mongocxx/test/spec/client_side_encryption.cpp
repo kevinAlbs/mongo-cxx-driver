cbe2de4f7502aef7cc6b79f34a47423911e85412
Kevin Albertson <kevin.albertson@mongodb.com>
2026-07-08 08:43:42 -0400

    apply cryptSharedLibPath in legacy CSFLE test runner

--- src/mongocxx/test/spec/client_side_encryption.cpp
+++ src/mongocxx/test/spec/client_side_encryption.cpp
@@ -165,7 +165,14 @@ void add_auto_encryption_opts(document::view test, options::client* client_opts)
         char* bypass_spawn = std::getenv("ENCRYPTION_TESTS_BYPASS_SPAWN");
         char* mongocryptd_path = std::getenv("MONGOCRYPTD_PATH");
 
-        if (bypass_spawn || mongocryptd_path) {
+        auto const shared_lib_path = std::getenv("CRYPT_SHARED_LIB_PATH");
+        if (shared_lib_path) {
+            using bsoncxx::builder::basic::kvp;
+            using bsoncxx::builder::basic::make_document;
+
+            auto_encrypt_opts.extra_options(
+                make_document(kvp("cryptSharedLibPath", shared_lib_path), kvp("cryptSharedLibRequired", true)));
+        } else if (bypass_spawn || mongocryptd_path) {
             auto cmd = bsoncxx::builder::basic::document{};
 
             if (bypass_spawn && strcmp(bypass_spawn, "TRUE") == 0) {
